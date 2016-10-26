289a1fd8fff21cbf3f7cc968eddfad230ee983dc
Daníel Örn <danielos15@ru.is>
2016-10-26 16:29:53 +0000

    added java files to start

---
+++ src/main/java/com/tictactoe/ticTacToe.java
@@ -0,0 +1,9 @@
+package com.tictactoe.ticTacToe
+
+public class ticTacToe {
+
+	public static void main(String [] args)
+	{
+
+	}
+}
---
+++ src/test/java/com/tictactoe/ticTacToeTest.java
@@ -0,0 +1,13 @@
+package com.tictactoe.ticTacToe;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ticTacToeTest {
+
+	@Test
+    public void firstTest() {
+        
+    }
+
+}
