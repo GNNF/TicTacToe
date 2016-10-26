125f2f28e10b196f8cfdd159276e3368cb9f2242
eyrunm <eyrun15@ru.is>
2016-10-26 19:00:29 +0000

    Started on Player class

---
+++ src/main/java/com/tictactoe/Player.java
@@ -0,0 +1,16 @@
+package com.tictactoe;
+
+public class Player{
+
+	private String name;
+	private char symbol;
+
+	public Player(){
+		name = "";
+	}
+	public String getName(){
+		return name;
+	}
+
+
+}
