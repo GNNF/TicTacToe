bf4811792ed67ff643d0b5e41eca350d2ccca379
Daníel Örn <danielos15@ru.is>
2016-10-26 19:13:36 +0000

    added board class

---
+++ src/test/java/com/tictactoe/boardTest.java
@@ -0,0 +1,13 @@
+package com.tictactoe;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BoardTest {
+
+	@Test
+    public void testIsOver() {
+        assertEquals(false, true);
+    }
+
+}
