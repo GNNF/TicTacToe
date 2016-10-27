584930cc25258a46bb183f6d9d38e5a2eddbc251
Daníel Örn <danielos15@ru.is>
2016-10-27 00:48:51 +0000

    board checkCell and isGameOver

--- src/main/java/com/tictactoe/Board.java
+++ src/main/java/com/tictactoe/Board.java
@@ -1,21 +1,45 @@
 package com.tictactoe;
 
 public class Board {
+	private final int MAX_SIZE = 9;
+	private final char ALLOWED_SYMBOLS[] = new char[] {'X', 'O'};
 	private char board[];
+	private int totalPlays;
 
 	Board() {
 		this.init();
 	}
 
 	public void init() {
-		board = new char[9];
-		for (int i = 0; i < 9; i++) {
+		board = new char[MAX_SIZE];
+		totalPlays = 0;
+		for (int i = 0; i < MAX_SIZE; i++) {
 			board[i] = (char)i;
 		}
 	}
 
-	public boolean isOver() {
-		return true;
+	public boolean checkCell(int cell, char symbol) {
+		if (cell < MAX_SIZE && cell >= 0) {
+			if (this.isAllowedSymbol(symbol)) {
+				board[cell] = symbol;
+				totalPlays++;
+				return true;
+			}
+		}
+		//Unable to check cell since its out of bounds
+		return false;
+	}
+
+	public boolean isGameOver() {
+		return totalPlays == 9;
+	}
+
+	private boolean isAllowedSymbol(char symbol) {
+		for(char allowedSymbol : ALLOWED_SYMBOLS) {
+			if (symbol == allowedSymbol)
+				return true;
+		}
+		return false;
 	}
 
 }
--- src/test/java/com/tictactoe/BoardTest.java
+++ src/test/java/com/tictactoe/BoardTest.java
@@ -6,9 +6,45 @@ import static org.junit.Assert.assertEquals;
 public class BoardTest {
 
 	@Test
-    public void testIsOver() {
+    public void testIsGameOver() {
     	Board board = new Board();
-        assertEquals(true, board.isOver());
+    	for (int i = 0; i < 9; i++) {
+    		board.checkCell(i, 'X');
+    	}
+        assertEquals(true, board.isGameOver());
+    }
+
+    @Test
+    public void testIsGameNotOver() {
+    	Board board = new Board();
+    	for (int i = 0; i < 5; i++) {
+    		board.checkCell(i, 'O');
+    	}
+        assertEquals(false, board.isGameOver());
+    }
+
+    @Test
+    public void testCheckCellOne() {
+    	Board board = new Board();
+    	assertEquals(true, board.checkCell(4, 'X'));
+    }
+
+	@Test
+    public void testCheckCellTwo() {
+    	Board board = new Board();
+    	assertEquals(false, board.checkCell(12, 'X'));
+    }
+
+    @Test
+    public void testCheckCellThree() {
+    	Board board = new Board();
+    	assertEquals(false, board.checkCell(4, 'g'));
+    }
+
+    @Test
+    public void testCheckCellFour() {
+    	Board board = new Board();
+    	assertEquals(true, board.checkCell(8, 'O'));
     }
 
 }
