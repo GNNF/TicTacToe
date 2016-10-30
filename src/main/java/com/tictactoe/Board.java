02ff60157732919f132c396b0a4dddd71d9a8da6
Egill Anfinnsson Heinesen <egill.heinesen@advania.is>
2016-10-30 21:45:52 +0000

    minor fixes

--- src/main/java/com/tictactoe/Board.java
+++ src/main/java/com/tictactoe/Board.java
@@ -1,14 +1,3 @@
-/**
-* Domain classes used to produce .....
-* <p>
-* These classes contain the ......
-* </p>
-*
-* @since 1.0
-* @author somebody
-* @version 1.0
-*/
-
 package com.tictactoe;
 
 public class Board {
